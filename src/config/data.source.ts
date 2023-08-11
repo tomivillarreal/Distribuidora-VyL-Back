ed74966db2b152c2965b4df00cbc3b9c284daf21
tomivillarreal <134805538+tomivillarreal@users.noreply.github.com>
2023-08-11 19:38:51 -0300

    docker-compose

--- src/config/data.source.ts
+++ src/config/data.source.ts
@@ -1,27 +1,27 @@
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { DataSource, DataSourceOptions, SimpleConsoleLogger } from "typeorm";
-import { SnakeNamingStrategy } from "typeorm-naming-strategies";
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 ConfigModule.forRoot({
-    envFilePath: `.${process.env.NODE_ENV}.env`,
+  envFilePath: `.${process.env.NODE_ENV}.env`,
 });
 
 const configService = new ConfigService();
-console.log(`.${process.env.NODE_ENV}.env`)
+console.log(`.${process.env.NODE_ENV}.env`);
 
 export const DataSourceConfig: DataSourceOptions = {
-    type: 'postgres',
-    host: configService.get('DB_HOST'),
-    port: configService.get('DB_PORT'),
-    username: configService.get('DB_USER'),
-    password: configService.get('DB_PASSWORD'),
-    database: configService.get('DB_NAME'),
-    entities: [__dirname + '/../**/**/*.entity{.ts,.js}'],
-    migrations: [__dirname + '/../../migrations/*{.ts,.js}'],
-    synchronize: false,
-    migrationsRun: true,
-    logging: false,
-    namingStrategy: new SnakeNamingStrategy(),
+  type: 'postgres',
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USER'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_NAME'),
+  entities: [__dirname + '/../**/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../../migrations/*{.ts,.js}'],
+  synchronize: false,
+  migrationsRun: true,
+  logging: false,
+  namingStrategy: new SnakeNamingStrategy(),
 };
 
 export const AppDS = new DataSource(DataSourceConfig);
